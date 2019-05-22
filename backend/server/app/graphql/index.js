fcd84c055a87f4c885118c2712b8d5619ec625a8
haotian <ritou11@gmail.com>
2019-05-23 00:29:09 +0800

    server graphql

--- backend/server/app/graphql/index.js
+++ backend/server/app/graphql/index.js
@@ -5,19 +5,14 @@ const { Comment } = require('../../models/comment');
 const { Post } = require('../../models/post');
 const { Profile } = require('../../models/profile');
 const { ProfilePubRecord } = require('../../models/profilePubRecord');
-const { project, resolvers } = require('./projection');
 
 const typeDefs = fs.readFileSync('./docs/wxPubAnal.graphql', 'utf8');
 
 const schema = makeExecutableSchema({
   typeDefs,
-  resolvers: _.merge(
-    resolvers, {
-    },
-  ),
+  resolvers:{},
 });
 
 module.exports = {
-  resolvers,
   schema,
 };

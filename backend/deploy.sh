6c1534b37c4d9fc65a9cfa8b7c0032b9d8f03ec0
haotian <ritou11@gmail.com>
2019-05-27 00:51:23 +0800

    deploy myConfig.json

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 eval $(docker-machine env aliyun)
 docker-machine scp -r ./conf/* aliyun:/root/letsencrypt/
+docker-machine scp ./crawler/myConfig.json aliyun:/root/
 docker-compose --file docker-compose-deploy.yml up -d

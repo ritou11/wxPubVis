30d403481325fb3f962f8b9e2294f073663fb0ac
hoiian <hoiian@users.noreply.github.com>
2019-06-05 02:13:32 +0800

    cancel \"

--- frontend/frontend_test/js/cloud.js
+++ frontend/frontend_test/js/cloud.js
@@ -4,25 +4,23 @@ var TARGET_ELEMENT_ID = '#cloud';
 d3.json(DATA_FILE_PATH).then(function (data) { // v5
 	console.log(data);
 	var h = 490;
-	var w = 600;
+	var w = 800;
 
 	var random = d3.randomIrwinHall(2);
 	var countMax = d3.max(data, function (d) {
 		return d.freq
 	});
 	var sizeScale = d3.scaleLinear().domain([0, countMax]).range([10, 100])
-//	var a = "sdjfj\"kw";
-//	data = data.replace(/"([^"]*)"/g,'');
-//	console.log(a);
+
 	var words = data.map(function (d) {
 		return {
-			text: d.name.replace(/＼"/g,''),
+			text: d.name.replace(/\"/g,''),
 			size: sizeScale(d.freq)
 		};
 		
 		
 	});
-	console.log(typeof(data));
+	console.log(Object.values(words));
 	
 	d3.layout.cloud().size([w, h])
 		.words(words)
